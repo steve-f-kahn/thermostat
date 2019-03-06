fc611eb212b08fa2054e31281adbffff4eb60202
Steve Kahn <steve@kahn.org.uk>
2019-03-06 16:48:44 +0000

    Added power saving mode off state and corresponding max tp

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,19 +2,24 @@ function Thermostat() {
   this.temp = 20;
   this.minimumTemperature = 10;
   this.powerSavingMode = true;
-  this.powerSavingModeMaxTemp = 25;
+  this.powerSavingModeOnMaxTemp = 25;
+  this.powerSavingModeOffMaxTemp = 32;
 };
 
 Thermostat.prototype.up = function (num) {
   var newTemperature = this.temp + num;
   if(this.powerSavingMode) {
-    newTemperature > this.powerSavingModeMaxTemp ? this.temp = 25 : this.temp += num;
+    newTemperature > this.powerSavingModeOnMaxTemp ? this.temp = this.powerSavingModeOnMaxTemp : this.temp += num;
   } else {
-    this.temp += num;
-  }
+    newTemperature > this.powerSavingModeOffMaxTemp ? this.temp = this.powerSavingModeOffMaxTemp : this.temp += num;
+  };
 };
 
 Thermostat.prototype.down = function (num) {
   var newTemperature = this.temp - num;
   newTemperature < this.minimumTemperature ? this.temp = 10 : this.temp -= num
+};
+
+Thermostat.prototype.togglePowerSavingMode = function () {
+  this.powerSavingMode ? this.powerSavingMode = false : this.powerSavingMode = true
 };
